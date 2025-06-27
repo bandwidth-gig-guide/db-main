29a47304ad8e30855de0ad1ccffeae98d8839cd1
MatthewLeigh <matthewcross97@gmail.com>
2025-06-28 00:28:16 +1000

    refactor: update create table statements to match erd-v1.0

--- create_tables.sql
+++ create_tables.sql
@@ -1,59 +1,111 @@
 
-CREATE TABLE IF NOT EXISTS User (
-    UserID INT NOT NULL AUTO_INCREMENT,
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+
+-- Types
+CREATE TYPE STATECODE AS ENUM (
+    'NSW',
+    'VIC',
+    'QLD',
+    'SA',
+    'WA',
+    'TAS',
+    'ACT',
+    'NT'
+);
+
+CREATE TYPE SOCIALPLATFORM AS ENUM (
+    'Website',
+    'Facebook',
+    'Twitter',
+    'Instagram',
+    'TikTok',
+    'SoundCloud',
+    'Spotify',
+    'Bandcamp',
+    'YouTube'
+);
+
+
+-- Main Tables
+CREATE TABLE Image (
+    ImageID UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    URL TEXT NOT NULL
+);
+
+CREATE TABLE Artist (
+    ArtistID UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    Title VARCHAR(100) NOT NULL,
+    Country VARCHAR(100) NOT NULL,
+    City VARCHAR(100) NOT NULL,
+    StateCode STATECODE NOT NULL,
+    YearFounded CHAR(4) NOT NULL,
+    Description TEXT NOT NULL,
+    SpotifyEmbedURL TEXT,
+    YoutubeEmbedURL TEXT
+);
+
+CREATE TABLE Venue (
+    VenueID UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    Title VARCHAR(100) NOT NULL,
+    StreetAddress VARCHAR(100) NOT NULL,
+    City VARCHAR(100) NOT NULL,
+    StateCode STATECODE NOT NULL,
+    PostCode INT(4) NOT NULL,
+    Description TEXT NOT NULL,
+    WebsiteURL TEXT,
+    PhoneNumber VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE VenueStage (
+    StageID UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    VenueID UUID REFERENCES Venue(VenueID) ON DELETE CASCADE,
+    Title VARCHAR(100) NOT NULL,
+    Description TEXT NOT NULL,
+    Capacity INT(6) NOT NULL
+);
+
+CREATE TABLE Event (
+    EventID UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    VenueID UUID REFERENCES Venue(VenueID) ON DELETE CASCADE,
+    StageID INT REFERENCES VenueStage(StageID) ON DELETE CASCADE,
+    Title TEXT NOT NULL,
+    Description TEXT NOT NULL,
+    StartDateTime TIMESTAMP NOT NULL,
+    EndDateTime TIMESTAMP NOT NULL,
+    OriginalPostURL TEXT NOT NULL,
+    TicketSaleURL TEXT
+);
+
+CREATE TABLE User (
+    UserID UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     FirstName VARCHAR(50) NOT NULL,
     LastName VARCHAR(50) NOT NULL,
-    Email VARCHAR(255) NOT NULL UNIQUE,
-    Password VARCHAR(50) NOT NULL,
-    Country VARCHAR(50) NOT NULL,
-    State VARCHAR(50) NOT NULL,
-    PRIMARY KEY (UserID)
-) ENGINE = InnoDB AUTO_INCREMENT = 1;
+    EmailHash VARCHAR(255) NOT NULL UNIQUE,
+    PasswordHash VARCHAR(255) NOT NULL,
+    Country VARCHAR(100) NOT NULL,
+    City VARCHAR(100) NOT NULL,
+    StateCode STATECODE NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS Venue (
-    VenueID INT NOT NULL AUTO_INCREMENT,
-    Title VARCHAR(255) NOT NULL,
-    Description TEXT,
-    StreetAddress VARCHAR(255) NOT NULL,
-    City VARCHAR(50) NOT NULL,
-    State VARCHAR(50) NOT NULL,
-    PostCode VARCHAR(10) NOT NULL,
-    Website VARCHAR(255),
-    Featured BOOLEAN NOT NULL,
-    PRIMARY KEY (VenueID)
-) ENGINE = InnoDB AUTO_INCREMENT = 1;
+-- Secondary Tables
 
-CREATE TABLE IF NOT EXISTS Stage (
-    StageID INT NOT NULL AUTO_INCREMENT,
-    VenueID INT NOT NULL,
-    Title VARCHAR(50) NOT NULL,
-    Description TEXT,
-    Capacity INT NOT NULL,
-    PRIMARY KEY (StageID),
-    FOREIGN KEY (VenueID) REFERENCES Venue(VenueID)
-) ENGINE = InnoDB AUTO_INCREMENT = 1;
+CREATE TABLE EventPerformance (
+    EventID UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    ArtistID UUID REFERENCES Artist(ArtistID) ON DELETE CASCADE,
+    SetListPosition INT(2) NOT NULL,
+    StartDateTime TIMESTAMP NOT NULL,
+    EndDateTime TIMESTAMP NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS VenueSocials (
-    VenueID INT NOT NULL,
-    Platform ENUM(
-        'Website',
-        'Facebook',
-        'Twitter',
-        'Instagram',
-        'TikTok',
-        'SoundCloud',
-        'Spotify',
-        'Bandcamp',
-        'YouTube'
-    ) NOT NULL,
-    Handle VARCHAR(255) NOT NULL,
-    URL VARCHAR(510) NOT NULL,
-    PRIMARY KEY (VenueID, Platform),
-    FOREIGN KEY (VenueID) REFERENCES Venue(VenueID)
-) ENGINE = InnoDB;
+CREATE TABLE EventPrice (
+    EventID UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    TicketType VARCHAR(50) PRIMARY KEY,
+    Price DECIMAL(10,2) NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS VenueOpeningHours (
-    VenueID INT NOT NULL,
+CREATE TABLE VenueOpeningHours (
+    VenueID UUID PRIMARY REFERENCES Venue(VenueID) ON DELETE CASCADE,
     MonOpen TIME,
     MonClose TIME,
     TueOpen TIME,
@@ -68,216 +120,146 @@ CREATE TABLE IF NOT EXISTS VenueOpeningHours (
     SatClose TIME,
     SunOpen TIME,
     SunClose TIME
-) ENGINE = InnoDB;
+);
 
-CREATE TABLE IF NOT EXISTS Artist (
-    ArtistID INT NOT NULL AUTO_INCREMENT,
-    Title VARCHAR(50) NOT NULL,
-    Country VARCHAR(50) NOT NULL,
-    State VARCHAR(50) NOT NULL,
-    YearFounded YEAR,
-    Description TEXT,
-    SpotifyEmbed TEXT,
-    YouTubeEmbed TEXT,
-    Featured BOOLEAN NOT NULL,
-    PRIMARY KEY (ArtistID)
-) ENGINE = InnoDB AUTO_INCREMENT = 1;
+-- Tertiary Tables : Image
 
-CREATE TABLE IF NOT EXISTS ArtistSocials (
-    ArtistID INT NOT NULL,
-    Platform ENUM(
-        'Website',
-        'Facebook',
-        'Twitter',
-        'Instagram',
-        'TikTok',
-        'SoundCloud',
-        'Spotify',
-        'Bandcamp',
-        'YouTube'
-    ) NOT NULL,
-    Handle VARCHAR(255) NOT NULL,
-    URL VARCHAR(510) NOT NULL,
-    PRIMARY KEY (ArtistID, Platform),
-    FOREIGN KEY (ArtistID) REFERENCES Artist(ArtistID)
-) ENGINE = InnoDB;
+CREATE TABLE UserImage (
+    UserID UUID PRIMARY REFERENCES User(UserID) ON DELETE CASCADE,
+    ImageID UUID PRIMARY REFERENCES Image(ImageID) ON DELETE CASCADE
+);
 
-CREATE TABLE IF NOT EXISTS Event (
-    EventID INT NOT NULL AUTO_INCREMENT,
-    VenueID INT NOT NULL,
-    StageID INT NOT NULL,
-    Title VARCHAR(50) NOT NULL,
-    Description TEXT,
-    Date DATE NOT NULL,
-    StartTime TIME NOT NULL,
-    EndTime TIME NOT NULL,
-    Featured BOOLEAN NOT NULL,
-    Verified BOOLEAN NOT NULL,
-    OriginalPostURL VARCHAR(510) NOT NULL,
-    TicketSaleURL VARCHAR(510) NOT NULL,
-    Hits INT NOT NULL DEFAULT 0,
-    PRIMARY KEY (EventID),
-    FOREIGN KEY (VenueID) REFERENCES Venue(VenueID),
-    FOREIGN KEY (VenueID, StageID) REFERENCES Stage(VenueID, StageID) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 1;
+-- Tertiary Tables : Artist
 
-CREATE TABLE IF NOT EXISTS EventPrice (
-    EventID INT NOT NULL,
-    TicketType VARCHAR(50) NOT NULL,
-    Price DECIMAL(10, 2) NOT NULL,
-    PRIMARY KEY (EventID, TicketType),
-    FOREIGN KEY (EventID) REFERENCES Event(EventID)
-) ENGINE = InnoDB;
+CREATE TABLE UserArtistSaved (
+    UserID UUID PRIMARY REFERENCES User(UserID) ON DELETE CASCADE,
+    ArtistID UUID PRIMARY REFERENCES Artist(ArtistID) ON DELETE CASCADE
+);
 
-CREATE TABLE IF NOT EXISTS Performance (
-    EventID INT NOT NULL,
-    ArtistID INT NOT NULL,
-    SetListPosition INT NOT NULL,
-    PerformanceStartTime TIME,
-    PRIMARY KEY (EventID, ArtistID),
-    FOREIGN KEY (EventID) REFERENCES Event(EventID),
-    FOREIGN KEY (ArtistID) REFERENCES Artist(ArtistID)
-) ENGINE = InnoDB;
+CREATE TABLE UserArtistViewed (
+    UserID UUID PRIMARY REFERENCES User(UserID) ON DELETE CASCADE,
+    ArtistID UUID PRIMARY REFERENCES Artist(ArtistID) ON DELETE CASCADE,
+    DateTimeViewed TIMESTAMP DEFAULT NOW()
+);
 
-CREATE TABLE IF NOT EXISTS UserSavedEvent (
-    UserID INT NOT NULL,
-    EventID INT NOT NULL,
-    PRIMARY KEY (UserID, EventID),
-    FOREIGN KEY (UserID) REFERENCES User(UserID),
-    FOREIGN KEY (EventID) REFERENCES Event(EventID)
-) ENGINE = InnoDB;
+CREATE TABLE ArtistUserComment (
+    CommentID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    ArtistID UUID REFERENCES Artist(ArtistID) ON DELETE CASCADE,
+    UserID UUID REFERENCES User(UserID) ON DELETE CASCADE,
+    DateTime TIMESTAMP DEFAULT NOW()
+);
 
-CREATE TABLE IF NOT EXISTS UserRecentlyViewedEvent (
-    UserID INT NOT NULL,
-    EventID INT NOT NULL,
-    DateTimeViewed DATETIME NOT NULL,
-    PRIMARY KEY (UserID, EventID),
-    FOREIGN KEY (UserID) REFERENCES User(UserID),
-    FOREIGN KEY (EventID) REFERENCES Event(EventID)
-) ENGINE = InnoDB;
+CREATE TABLE ArtistUserCommentLike (
+    CommentID UUID PRIMARY REFERENCES ArtistUserComment(CommentID) ON DELETE CASCADE,
+    UserID UUID PRIMARY REFERENCES User(UserID) ON DELETE CASCADE
+);
 
-CREATE TABLE IF NOT EXISTS UserFavouriteArtist (
-    UserID INT NOT NULL,
-    ArtistID INT NOT NULL,
-    PRIMARY KEY (UserID, ArtistID),
-    FOREIGN KEY (UserID) REFERENCES User(UserID),
-    FOREIGN KEY (ArtistID) REFERENCES Artist(ArtistID)
-) ENGINE = InnoDB;
+CREATE TABLE ArtistSocial (
+    ArtistID UUID PRIMARY REFERENCES Artist(ArtistID) ON DELETE CASCADE,
+    SocialPlatform SOCIALPLATFORM PRIMARY KEY,
+    Handle VARCHAR(100) NOT NULL,
+    URL TEXT NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS UserFavouriteVenue (
-    UserID INT NOT NULL,
-    VenueID INT NOT NULL,
-    PRIMARY KEY (UserID, VenueID),
-    FOREIGN KEY (UserID) REFERENCES User(UserID),
-    FOREIGN KEY (VenueID) REFERENCES Venue(VenueID)
-) ENGINE = InnoDB;
+CREATE TABLE ArtistFeatured (
+    ArtistID UUID PRIMARY REFERENCES Artist(ArtistID) ON DELETE CASCADE,
+    ImageID UUID REFERENCES Image(ImageID) ON DELETE CASCADE,
+    StartDateTime TIMESTAMP NOT NULL,
+    EndDateTime TIMESTAMP NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS Image (
-    ImageID INT NOT NULL AUTO_INCREMENT,
-    URL VARCHAR(510) NOT NULL,
-    PRIMARY KEY (ImageID)
-) ENGINE = InnoDB;
+CREATE TABLE ArtistImage (
+    ArtistID UUID PRIMARY REFERENCES Artist(ArtistID) ON DELETE CASCADE,
+    ImageID UUID PRIMARY REFERENCES Image(ImageID) ON DELETE CASCADE,
+    DisplayOrder INT(1) PRIMARY KEY
+);
 
-CREATE TABLE IF NOT EXISTS EventImage (
-    EventID INT NOT NULL,
-    ImageID INT NOT NULL,
-    DisplayOrder INT NOT NULL,
-    PRIMARY KEY (EventID, ImageID),
-    FOREIGN KEY (EventID) REFERENCES Event(EventID),
-    FOREIGN KEY (ImageID) References Image(ImageID)
-) ENGINE = InnoDB;
+-- Tertiary Tables : Event
 
-CREATE TABLE IF NOT EXISTS VenueImage (
-    VenueID INT NOT NULL,
-    ImageID INT NOT NULL,
-    DisplayOrder INT NOT NULL,
-    PRIMARY KEY (VenueID, ImageID),
-    FOREIGN KEY (VenueID) REFERENCES Venue(VenueID),
-    FOREIGN KEY (ImageID) References Image(ImageID)
-) ENGINE = InnoDB;
+CREATE TABLE UserEventSaved (
+    UserID UUID PRIMARY REFERENCES User(UserID) ON DELETE CASCADE,
+    EventID UUID PRIMARY REFERENCES Event(EventID) ON DELETE CASCADE
+);
 
-CREATE TABLE IF NOT EXISTS ArtistImage (
-    ArtistID INT NOT NULL,
-    ImageID INT NOT NULL,
-    DisplayOrder INT NOT NULL,
-    PRIMARY KEY (ArtistID, ImageID),
-    FOREIGN KEY (ArtistID) REFERENCES Artist(ArtistID),
-    FOREIGN KEY (ImageID) REFERENCES Image(ImageID)
-) ENGINE = InnoDB;
+CREATE TABLE UserEventViewed (
+    UserID UUID PRIMARY REFERENCES User(UserID) ON DELETE CASCADE,
+    EventID UUID PRIMARY REFERENCES Event(EventID) ON DELETE CASCADE,
+    DateTimeViewed TIMESTAMP DEFAULT NOW()
+);
 
-CREATE TABLE IF NOT EXISTS UserImage (
-    UserID INT NOT NULL,
-    ImageID INT NOT NULL,
-    PRIMARY KEY (UserID, ImageID),
-    FOREIGN KEY (UserID) REFERENCES User(UserID),
-    FOREIGN KEY (ImageID) REFERENCES Image(ImageID)
-) ENGINE = InnoDB;
+CREATE TABLE EventUserComment (
+    CommentID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    EventID UUID REFERENCES Event(EventID) ON DELETE CASCADE,
+    UserID UUID REFERENCES User(UserID) ON DELETE CASCADE,
+    DateTime TIMESTAMP DEFAULT NOW()
+);
 
-CREATE TABLE IF NOT EXISTS Banner (
-    BannerID INT NOT NULL,
-    ImageID INT NOT NULL,
-    FocusType ENUM('event', 'artist', 'venue') NOT NULL,
-    FocusID INT NOT NULL,
-    PRIMARY KEY (BannerID, ImageID),
-    FOREIGN KEY (ImageID) REFERENCES Image(ImageID)
-) ENGINE = InnoDB;
+CREATE TABLE EventUserCommentLike (
+    CommentID UUID PRIMARY REFERENCES EventUserComment(CommentID) ON DELETE CASCADE,
+    UserID UUID PRIMARY REFERENCES User(UserID) ON DELETE CASCADE
+);
 
-CREATE TABLE IF NOT EXISTS EventComment (
-    EventCommentID INT NOT NULL AUTO_INCREMENT,
-    EventID INT NOT NULL,
-    UserID INT NOT NULL,
-    DateTime DateTime NOT NULL,
-    Comment TEXT NOT NULL,
-    IsEdited BOOL DEFAULT 0,
-    PRIMARY KEY (EventCommentID),
-    FOREIGN KEY (EventID) REFERENCES Event(EventID),
-    FOREIGN KEY (UserID) REFERENCES User(UserID)
-) ENGINE = InnoDB;
+CREATE TABLE EventSocial (
+    EventID UUID PRIMARY REFERENCES Event(EventID) ON DELETE CASCADE,
+    SocialPlatform SOCIALPLATFORM PRIMARY KEY,
+    Handle VARCHAR(100) NOT NULL,
+    URL TEXT NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS ArtistComment (
-    ArtistCommentID INT NOT NULL AUTO_INCREMENT,
-    ArtistID INT NOT NULL,
-    UserID INT NOT NULL,
-    DateTime DateTime NOT NULL,
-    Comment TEXT NOT NULL,
-    IsEdited BOOL DEFAULT 0,
-    PRIMARY KEY (ArtistCommentID),
-    FOREIGN KEY (ArtistID) REFERENCES Artist(ArtistID),
-    FOREIGN KEY (UserID) REFERENCES User(UserID)
-) ENGINE = InnoDB;
+CREATE TABLE EventFeatured (
+    EventID UUID PRIMARY REFERENCES Event(EventID) ON DELETE CASCADE,
+    ImageID UUID REFERENCES Image(ImageID) ON DELETE CASCADE,
+    StartDateTime TIMESTAMP NOT NULL,
+    EndDateTime TIMESTAMP NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS VenueComment (
-    VenueCommentID INT NOT NULL AUTO_INCREMENT,
-    VenueID INT NOT NULL,
-    UserID INT NOT NULL,
-    DateTime DateTime NOT NULL,
-    Comment TEXT NOT NULL,
-    IsEdited BOOL DEFAULT 0,
-    PRIMARY KEY (VenueCommentID),
-    FOREIGN KEY (VenueID) REFERENCES Venue(VenueID),
-    FOREIGN KEY (UserID) REFERENCES User(UserID)
-) ENGINE = InnoDB;
+CREATE TABLE EventImage (
+    EventID UUID PRIMARY REFERENCES Event(EventID) ON DELETE CASCADE,
+    ImageID UUID PRIMARY REFERENCES Image(ImageID) ON DELETE CASCADE,
+    DisplayOrder INT(1) PRIMARY KEY
+);
 
-CREATE TABLE IF NOT EXISTS EventCommentLike (
-    EventCommentID INT NOT NULL,
-    UserID INT NOT NULL,
-    PRIMARY KEY (EventCommentID, UserID),
-    FOREIGN KEY (EventCommentID) REFERENCES EventComment(EventCommentID) ON DELETE CASCADE,
-    FOREIGN KEY (UserID) REFERENCES User(UserID)
-) ENGINE = InnoDB;
+-- Tertiary Tables : Venue
 
-CREATE TABLE IF NOT EXISTS ArtistCommentLike (
-    ArtistCommentID INT NOT NULL,
-    UserID INT NOT NULL,
-    PRIMARY KEY (ArtistCommentID, UserID),
-    FOREIGN KEY (ArtistCommentID) REFERENCES ArtistComment(ArtistCommentID) ON DELETE CASCADE,
-    FOREIGN KEY (UserID) REFERENCES User(UserID)
-) ENGINE = InnoDB;
+CREATE TABLE UserVenueSaved (
+    UserID UUID PRIMARY REFERENCES User(UserID) ON DELETE CASCADE,
+    VenueID UUID PRIMARY REFERENCES Venue(VenueID) ON DELETE CASCADE
+);
 
-CREATE TABLE IF NOT EXISTS VenueCommentLike (
-    VenueCommentID INT NOT NULL,
-    UserID INT NOT NULL,
-    PRIMARY KEY (VenueCommentID, UserID),
-    FOREIGN KEY (VenueCommentID) REFERENCES VenueComment(VenueCommentID) ON DELETE CASCADE,
-    FOREIGN KEY (UserID) REFERENCES User(UserID)
-) ENGINE = InnoDB;
+CREATE TABLE UserVenueViewed (
+    UserID UUID PRIMARY REFERENCES User(UserID) ON DELETE CASCADE,
+    VenueID UUID PRIMARY REFERENCES Venue(VenueID) ON DELETE CASCADE,
+    DateTimeViewed TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE VenueUserComment (
+    CommentID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    VenueID UUID REFERENCES Venue(VenueID) ON DELETE CASCADE,
+    UserID UUID REFERENCES User(UserID) ON DELETE CASCADE,
+    DateTime TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE VenueUserCommentLike (
+    CommentID UUID PRIMARY REFERENCES VenueUserComment(CommentID) ON DELETE CASCADE,
+    UserID UUID PRIMARY REFERENCES User(UserID) ON DELETE CASCADE
+);
+
+CREATE TABLE VenueSocial (
+    VenueID UUID PRIMARY REFERENCES Venue(VenueID) ON DELETE CASCADE,
+    SocialPlatform SOCIALPLATFORM PRIMARY KEY,
+    Handle VARCHAR(100) NOT NULL,
+    URL TEXT NOT NULL
+);
+
+CREATE TABLE VenueFeatured (
+    VenueID UUID PRIMARY REFERENCES Venue(VenueID) ON DELETE CASCADE,
+    ImageID UUID REFERENCES Image(ImageID) ON DELETE CASCADE,
+    StartDateTime TIMESTAMP NOT NULL,
+    EndDateTime TIMESTAMP NOT NULL
+);
+
+CREATE TABLE VenueImage (
+    VenueID UUID PRIMARY REFERENCES Venue(VenueID) ON DELETE CASCADE,
+    ImageID UUID PRIMARY REFERENCES Image(ImageID) ON DELETE CASCADE,
+    DisplayOrder INT(1) PRIMARY KEY
+);
