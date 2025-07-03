fbc79adf2d4a8ffd7f1037d7f3afcbbcb7ef8b09
Matthew Cross <119134131+MatthewLeigh@users.noreply.github.com>
2025-07-03 13:35:00 +1000

    refactor: add display order attribute

--- statements/create-table/round-2/AccountImage.sql
+++ statements/create-table/round-2/AccountImage.sql
@@ -1,7 +1,8 @@
 CREATE TABLE AccountImage (
     AccountID UUID,
     ImageID UUID,
-    PRIMARY KEY (AccountID),
+    DisplayOrder INTEGER,
+    PRIMARY KEY (AccountID, DisplayOrder),
     FOREIGN KEY (AccountID) REFERENCES Account(AccountID) ON DELETE CASCADE,
     FOREIGN KEY (ImageID) REFERENCES Image(ImageID) ON DELETE CASCADE
 );
