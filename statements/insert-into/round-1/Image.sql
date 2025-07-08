35abaeee20c7449253528a5192aece214712e64a
MatthewLeigh <matthewcross97@gmail.com>
2025-07-08 11:18:15 +1000

    misc: add images for new artists

--- statements/insert-into/round-1/Image.sql
+++ statements/insert-into/round-1/Image.sql
@@ -1,9 +1,14 @@
-INSERT INTO Image (URL) 
+INSERT INTO Image (ImageID, URL) 
 VALUES 
-    ('https://www.basicstuffmagazine.com/wp-content/uploads/2020/04/N1Ynur1-.jpg'), -- The Amazing Devil
-    ('https://www.philadelphiamusicalliance.org/wp-content/uploads/the-roots-02.jpg'), -- The Roots
-    ('https://www.mysticsons.com/sites/default/files/styles/large/public/19613/feature/unnamed-700.jpg?itok=kd5BJGra'),-- Tome Misch
-    ('https://media.timeout.com/images/103864820/750/422/image.jpg'), -- The Gasometer Hotel
-    ('https://www.neolutionesport.com/wp-content/uploads/2017/03/default-avatar-knives-ninja.png'), -- Matthew Cross
-    ('https://i.etsystatic.com/51700482/r/il/90cddf/5967617983/il_fullxfull.5967617983_gdx0.jpg'), -- Event Image
-    ('https://i0.wp.com/liveforlivemusic.com/wp-content/uploads/2024/03/rose-on-the-river-fbcover.jpg?resize=740%2C390&ssl=1'); -- Event Banner
+    ('a1e2b3c4-d5e6-47f7-98h9-i0j1k2l3m4n5', 'https://www.basicstuffmagazine.com/wp-content/uploads/2020/04/N1Ynur1-.jpg'), -- The Amazing Devil
+    ('b2f3c4d5-e6f7-48g8-h9i0-j1k2l3m4n5o6', 'https://www.philadelphiamusicalliance.org/wp-content/uploads/the-roots-02.jpg'), -- The Roots
+    ('c3g4d5e6-f7g8-49h9-i0j1-k2l3m4n5o6p7', 'https://www.mysticsons.com/sites/default/files/styles/large/public/19613/feature/unnamed-700.jpg?itok=kd5BJGra'), -- Tom Misch
+    ('h8l9i0j1-k2l3-44m4-n5o6-p7q8r9s0t1u2', 'https://i1.sndcdn.com/avatars-ySVPHIyw8Qbp86uk-YXdGgg-t1080x1080.jpg'), -- Rhysics
+    ('i9m0j1k2-l3m4-45n5-o6p7-q8r9s0t1u2v3', 'https://www.rollingstone.com/wp-content/uploads/2019/04/tame-impala-tour.jpg?w=1581&h=1054&crop=1'), -- Tame Impala
+    ('j0n1k2l3-m4n5-46o6-p7q8-r9s0t1u2v3w4', 'https://images-r2.thebrag.com/rs/uploads/2021/10/gang-of-youths.jpg?resize=940,705&w=940'), -- Gang of Youths
+    ('k1o2l3m4-n5o6-47p7-q8r9-s0t1u2v3w4x5', 'https://res.akamaized.net/domain/image/fetch/t_web/https://static.domain.com.au/twr/production/uploads/2017/07/20205756/The-Teskey-Brothers5-web-e1500519823818.jpg'), -- The Teskey Brothers
+    ('l2p3m4n5-o6p7-48q8-r9s0-t1u2v3w4x5y6', 'https://live-production.wcms.abc-cdn.net.au/62f792575b559ddb1ebb35de0bca73d5?impolicy=wcms_crop_resize&cropH=562&cropW=1000&xPos=0&yPos=0&width=862&height=485'), -- Ball Park Music
+    ('d4h5e6f7-g8h9-40i0-j1k2-l3m4n5o6p7q8', 'https://media.timeout.com/images/103864820/750/422/image.jpg'), -- The Gasometer Hotel
+    ('e5i6f7g8-h9i0-41j1-k2l3-m4n5o6p7q8r9', 'https://www.neolutionesport.com/wp-content/uploads/2017/03/default-avatar-knives-ninja.png'), -- Matthew Cross
+    ('f6j7g8h9-i0j1-42k2-l3m4-n5o6p7q8r9s0', 'https://i.etsystatic.com/51700482/r/il/90cddf/5967617983/il_fullxfull.5967617983_gdx0.jpg'), -- Event Image
+    ('g7k8h9i0-j1k2-43l3-m4n5-o6p7q8r9s0t1', 'https://i0.wp.com/liveforlivemusic.com/wp-content/uploads/2024/03/rose-on-the-river-fbcover.jpg?resize=740%2C390&ssl=1'); -- Event Banner
