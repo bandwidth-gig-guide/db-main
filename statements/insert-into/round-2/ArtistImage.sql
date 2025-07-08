281375a6048e02a3aca7b3fc02cc263ca9102bff
MatthewLeigh <matthewcross97@gmail.com>
2025-07-08 11:20:00 +1000

    misc: use ids for image assignment

--- statements/insert-into/round-2/ArtistImage.sql
+++ statements/insert-into/round-2/ArtistImage.sql
@@ -1,17 +1,10 @@
 INSERT INTO ArtistImage (ArtistID, ImageID, DisplayOrder)
 VALUES
-(
-    (SELECT ArtistID FROM Artist WHERE Title = 'The Roots'),
-    (SELECT ImageID FROM Image WHERE URL = 'https://www.philadelphiamusicalliance.org/wp-content/uploads/the-roots-02.jpg'),
-    1
-),
-(
-    (SELECT ArtistID FROM Artist WHERE Title = 'The Amazing Devil'),
-    (SELECT ImageID FROM Image WHERE URL = 'https://www.basicstuffmagazine.com/wp-content/uploads/2020/04/N1Ynur1-.jpg'),
-    1
-),
-(
-    (SELECT ArtistID FROM Artist WHERE Title = 'Tom Misch'),
-    (SELECT ImageID FROM Image WHERE URL = 'https://www.mysticsons.com/sites/default/files/styles/large/public/19613/feature/unnamed-700.jpg?itok=kd5BJGra'),
-    1
-);
+    ('c1f2e3d4-5b6a-7c8d-9e0f-1a2b3c4d5e6f', 'b2f3c4d5-e6f7-48g8-h9i0-j1k2l3m4n5o6', 1), -- The Roots
+    ('b7e8e7e2-7c6a-4e2e-9c2a-1a2b3c4d5e6f', 'a1e2b3c4-d5e6-47f7-98h9-i0j1k2l3m4n5', 1), -- The Amazing Devil
+    ('d2e3f4a5-b6c7-8d9e-0f1a-2b3c4d5e6f7a', 'c3g4d5e6-f7g8-49h9-i0j1-k2l3m4n5o6p7', 1), -- Tom Misch
+    ('e3f4a5b6-c7d8-9e0f-1a2b-3c4d5e6f7a8b', 'h8l9i0j1-k2l3-44m4-n5o6-p7q8r9s0t1u2', 1), -- Rhsyics
+    ('f4a5b6c7-d8e9-0f1a-2b3c-4d5e6f7a8b9c', 'i9m0j1k2-l3m4-45n5-o6p7-q8r9s0t1u2v3', 1), -- Tame Impala
+    ('a5b6c7d8-e9f0-1a2b-3c4d-5e6f7a8b9c0d', 'j0n1k2l3-m4n5-46o6-p7q8-r9s0t1u2v3w4', 1), -- Gang of Youths
+    ('b6c7d8e9-f0a1-2b3c-4d5e-6f7a8b9c0d1e', 'k1o2l3m4-n5o6-47p7-q8r9-s0t1u2v3w4x5', 1), -- The Teskey Brothers
+    ('c7d8e9f0-a1b2-3c4d-5e6f-7a8b9c0d1e2f', 'l2p3m4n5-o6p7-48q8-r9s0-t1u2v3w4x5y6', 1); -- Ball Park Music
