ffe4bc23a8e139c4fff2f66a89e093c794e3f50f
MatthewLeigh <matthewcross97@gmail.com>
2025-07-16 09:39:04 +1000

    fix: make type name uppercase

--- statements/create-table/round-2/StoreType.sql
+++ statements/create-table/round-2/StoreType.sql
@@ -1,6 +1,6 @@
 CREATE TABLE StoreType (
     StoreID UUID,
-    Type TYPEStore NOT NULL,
+    Type TYPESTORE NOT NULL,
     PRIMARY KEY (StoreID, Type),
     FOREIGN KEY (StoreID) REFERENCES Store(StoreID) ON DELETE CASCADE
 );
