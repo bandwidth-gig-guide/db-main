e9c8d3880d4d7dbbada5002975cb646f3a7b58f7
MatthewLeigh <matthewcross97@gmail.com>
2025-07-15 10:28:23 +1000

    feat: add TeacherLesson table

---
+++ statements/create-table/round-2/TeacherLesson.sql
@@ -0,0 +1,11 @@
+CREATE TABLE TeacherLesson(
+    TeacherID UUID,
+    Instrument INSTRUMENT NOT NULL,
+    LessonType TYPELESSON NOT NULL,
+    DurationMins INTEGER NOT NULL
+    PricePerLesson DECIMAL(10,2),
+    ExperienceLevelMin EXPERIENCELEVEL,
+    ExperienceLevelMax EXPERIENCELEVEL,
+    PRIMARY KEY (TeacherID, Instrument, LessonType, DurationMins),
+    FOREIGN KEY (TeacherID) REFERENCES Teacher(TeacherID) ON DELETE CASCADE
+);
