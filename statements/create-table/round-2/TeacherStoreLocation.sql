620409850f843306c8bdd3d5ebda88ceecbccf04
MatthewLeigh <matthewcross97@gmail.com>
2025-07-16 09:39:43 +1000

    fix: reference the Teacher table

--- statements/create-table/round-2/TeacherStoreLocation.sql
+++ statements/create-table/round-2/TeacherStoreLocation.sql
@@ -1,7 +1,7 @@
 CREATE TABLE TeacherStoreLocation (
     TeacherID UUID,
     StoreLocationID UUID,
-    PRIMARY KEY (StoreID, StoreLocationID),
-    FOREIGN KEY (StoreID) REFERENCES Store(StoreID) ON DELETE CASCADE,
+    PRIMARY KEY (TeacherID, StoreLocationID),
+    FOREIGN KEY (TeacherID) REFERENCES Teacher(TeacherID) ON DELETE CASCADE,
     FOREIGN KEY (StoreLocationID) REFERENCES StoreLocation(StoreLocationID) ON DELETE CASCADE
 );
