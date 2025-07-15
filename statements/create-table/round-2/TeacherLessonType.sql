a292f7e97cba04d7a8f98f4f3ef7483de2107604
MatthewLeigh <matthewcross97@gmail.com>
2025-07-15 10:29:21 +1000

    feat: remove TeacherLessonType table
    refactored to now be a part of the the TeacherLesson table

--- statements/create-table/round-2/TeacherLessonType.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE TeacherLessonType (
-    TeacherID UUID,
-    Type TYPELESSON NOT NULL,
-    PRIMARY KEY (TeacherID, Type),
-    FOREIGN KEY (TeacherID) REFERENCES Teacher(TeacherID) ON DELETE CASCADE
-);
