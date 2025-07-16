0769a45a837cc2bcdf3f3b81b274b4d7effbd544
MatthewLeigh <matthewcross97@gmail.com>
2025-07-16 12:36:16 +1000

    fix: add quotes around uuid + more inserts

--- statements/insert-into/02-secondary/event/EventImage.sql
+++ statements/insert-into/02-secondary/event/EventImage.sql
@@ -1,3 +1,6 @@
 INSERT INTO EventImage (EventID, ImageID, DisplayOrder)
 VALUES
-    ('f81d4fae-7dec-11d0-a765-00a0c91e6bf6', c0b1a2d3-e4f5-6789-abcd-ef0123456789, 1); -- Sunset Groove Festival
+    ('f81d4fae-7dec-11d0-a765-00a0c91e6bf6', 'c0b1a2d3-e4f5-6789-abcd-ef0123456789', 1), -- Sunset Groove Festival
+    ('f8d12c6f0-964d-4f71-9f62-f4e9a64a7e2d', 'c0b1a2d3-e4f5-6789-abcd-ef0123456789', 1), -- Acoustic Evenings
+    ('a4e5f6d7-c89b-4a0e-9f2e-6e8a1b7c3d4f', 'c0b1a2d3-e4f5-6789-abcd-ef0123456789', 1); -- Electric Pulse Night
+    -- Soulful Sunday Sessions (Test no image)
