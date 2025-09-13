78669a2de178ecb4ee3e12ad1c736f78949e23cc
MatthewLeigh <matthewcross97@gmail.com>
2025-09-13 21:45:03 +1000

    misc: expand venue dummy data

---
+++ statements/insert-into/02-secondary/venue/VenueSocial.sql
@@ -0,0 +1,60 @@
+INSERT INTO VenueSocial (VenueID, SocialPlatform, Handle, URL) VALUES
+-- The Gasometer Hotel (4 socials)
+('e3b0c442-98fc-1c14-9afb-4c1f1b6e8b8a', 'Facebook', 'GasometerHotelOfficial', 'https://www.facebook.com/GasometerHotelOfficial'),
+('e3b0c442-98fc-1c14-9afb-4c1f1b6e8b8a', 'Instagram', 'gasometer_hotel', 'https://www.instagram.com/gasometer_hotel'),
+('e3b0c442-98fc-1c14-9afb-4c1f1b6e8b8a', 'Twitter', '@GasometerLive', 'https://twitter.com/GasometerLive'),
+('e3b0c442-98fc-1c14-9afb-4c1f1b6e8b8a', 'TikTok', 'gasometer_hotel_vids', 'https://www.tiktok.com/@gasometer_hotel_vids'),
+
+-- Corner Hotel (5 socials)
+('3f6a56b1-4d3a-4d26-8f6a-f9e68a03a2d7', 'Facebook', 'CornerHotelMelb', 'https://www.facebook.com/CornerHotelMelb'),
+('3f6a56b1-4d3a-4d26-8f6a-f9e68a03a2d7', 'Instagram', 'cornerhotel_aus', 'https://www.instagram.com/cornerhotel_aus'),
+('3f6a56b1-4d3a-4d26-8f6a-f9e68a03a2d7', 'Twitter', '@CornerHotelEvents', 'https://twitter.com/CornerHotelEvents'),
+('3f6a56b1-4d3a-4d26-8f6a-f9e68a03a2d7', 'TikTok', 'cornerhotel_music', 'https://www.tiktok.com/@cornerhotel_music'),
+('3f6a56b1-4d3a-4d26-8f6a-f9e68a03a2d7', 'YouTube', 'CornerHotelOfficial', 'https://www.youtube.com/@CornerHotelOfficial'),
+
+-- Northcote Social Club (3 socials)
+('7f8eec22-84af-49ef-987d-b8f7c79a16c4', 'Facebook', 'NorthcoteSocialClubOfficial', 'https://www.facebook.com/NorthcoteSocialClubOfficial'),
+('7f8eec22-84af-49ef-987d-b8f7c79a16c4', 'Instagram', 'northcotesocial', 'https://www.instagram.com/northcotesocial'),
+('7f8eec22-84af-49ef-987d-b8f7c79a16c4', 'Twitter', '@NorthcoteLive', 'https://twitter.com/NorthcoteLive'),
+
+-- The Tote (2 socials)
+('1a3c5e7f-9b2d-4c1e-bf4e-82f54c6a47e3', 'Facebook', 'ToteHotel', 'https://www.facebook.com/ToteHotel'),
+('1a3c5e7f-9b2d-4c1e-bf4e-82f54c6a47e3', 'Instagram', 'thetote_melb', 'https://www.instagram.com/thetote_melb'),
+
+-- Howler (3 socials)
+('d6b74a82-8c5a-41ac-bf29-72591c94f83d', 'Facebook', 'HowlerBrunswickMelb', 'https://www.facebook.com/HowlerBrunswickMelb'),
+('d6b74a82-8c5a-41ac-bf29-72591c94f83d', 'Instagram', 'howler_brunswick', 'https://www.instagram.com/howler_brunswick'),
+('d6b74a82-8c5a-41ac-bf29-72591c94f83d', 'TikTok', 'howler_melb', 'https://www.tiktok.com/@howler_melb'),
+
+-- The Evelyn Hotel (4 socials)
+('4e83c251-56d2-4f8e-8dc0-367e2e87e7d9', 'Facebook', 'EvelynHotelFitzroy', 'https://www.facebook.com/EvelynHotelFitzroy'),
+('4e83c251-56d2-4f8e-8dc0-367e2e87e7d9', 'Instagram', 'evelynhotel_aus', 'https://www.instagram.com/evelynhotel_aus'),
+('4e83c251-56d2-4f8e-8dc0-367e2e87e7d9', 'Twitter', '@EvelynLive', 'https://twitter.com/EvelynLive'),
+('4e83c251-56d2-4f8e-8dc0-367e2e87e7d9', 'YouTube', 'EvelynHotelOfficial', 'https://www.youtube.com/@EvelynHotelOfficial'),
+
+-- The Forum (5 socials)
+('7f2c81f9-0ad9-4a5f-9a08-03f1d7a54021', 'Facebook', 'ForumMelbourneOfficial', 'https://www.facebook.com/ForumMelbourneOfficial'),
+('7f2c81f9-0ad9-4a5f-9a08-03f1d7a54021', 'Instagram', 'forum_melb', 'https://www.instagram.com/forum_melb'),
+('7f2c81f9-0ad9-4a5f-9a08-03f1d7a54021', 'Twitter', '@ForumEventsMelb', 'https://twitter.com/ForumEventsMelb'),
+('7f2c81f9-0ad9-4a5f-9a08-03f1d7a54021', 'TikTok', 'forummelbourneclips', 'https://www.tiktok.com/@forummelbourneclips'),
+('7f2c81f9-0ad9-4a5f-9a08-03f1d7a54021', 'YouTube', 'ForumMelbourne', 'https://www.youtube.com/@ForumMelbourne'),
+
+-- The Night Cat (2 socials)
+('9e8b39f1-2d4c-4ac0-8d8c-45981c2a8f33', 'Facebook', 'NightCatFitzroy', 'https://www.facebook.com/NightCatFitzroy'),
+('9e8b39f1-2d4c-4ac0-8d8c-45981c2a8f33', 'Instagram', 'thenightcat_melb', 'https://www.instagram.com/thenightcat_melb'),
+
+-- Max Watt's House of Music (3 socials)
+('abf3c6e4-22b1-4c63-91f1-40e62e5e9a74', 'Facebook', 'MaxWattsMelbourne', 'https://www.facebook.com/MaxWattsMelbourne'),
+('abf3c6e4-22b1-4c63-91f1-40e62e5e9a74', 'Instagram', 'maxwatts_melb', 'https://www.instagram.com/maxwatts_melb'),
+('abf3c6e4-22b1-4c63-91f1-40e62e5e9a74', 'Twitter', '@MaxWattsLive', 'https://twitter.com/MaxWattsLive'),
+
+-- The Workers Club (4 socials)
+('ce7b6125-bf7f-4c7d-a9de-0a823f13925e', 'Facebook', 'WorkersClubMelb', 'https://www.facebook.com/WorkersClubMelb'),
+('ce7b6125-bf7f-4c7d-a9de-0a823f13925e', 'Instagram', 'theworkersclub_melb', 'https://www.instagram.com/theworkersclub_melb'),
+('ce7b6125-bf7f-4c7d-a9de-0a823f13925e', 'Twitter', '@WorkersClubLive', 'https://twitter.com/WorkersClubLive'),
+('ce7b6125-bf7f-4c7d-a9de-0a823f13925e', 'YouTube', 'WorkersClubOfficial', 'https://www.youtube.com/@WorkersClubOfficial'),
+
+-- The Espy (3 socials)
+('f5a91d77-bc94-49de-b627-6c0e01f87363', 'Facebook', 'EspyStKildaOfficial', 'https://www.facebook.com/EspyStKildaOfficial'),
+('f5a91d77-bc94-49de-b627-6c0e01f87363', 'Instagram', 'espystkilda_live', 'https://www.instagram.com/espystkilda_live'),
+('f5a91d77-bc94-49de-b627-6c0e01f87363', 'Twitter', '@EspyLive', 'https://twitter.com/EspyLive');
---
+++ statements/insert-into/02-secondary/venue/VenueTag.sql
@@ -0,0 +1,57 @@
+INSERT INTO VenueTag (VenueID, Tag) VALUES
+-- The Gasometer Hotel
+('e3b0c442-98fc-1c14-9afb-4c1f1b6e8b8a', 'Rock'),
+('e3b0c442-98fc-1c14-9afb-4c1f1b6e8b8a', 'Indie'),
+('e3b0c442-98fc-1c14-9afb-4c1f1b6e8b8a', 'Electronic'),
+('e3b0c442-98fc-1c14-9afb-4c1f1b6e8b8a', 'Folk'),
+
+-- Corner Hotel
+('3f6a56b1-4d3a-4d26-8f6a-f9e68a03a2d7', 'Rock'),
+('3f6a56b1-4d3a-4d26-8f6a-f9e68a03a2d7', 'Alternative'),
+('3f6a56b1-4d3a-4d26-8f6a-f9e68a03a2d7', 'Indie'),
+
+-- Northcote Social Club
+('7f8eec22-84af-49ef-987d-b8f7c79a16c4', 'Indie'),
+('7f8eec22-84af-49ef-987d-b8f7c79a16c4', 'Funk'),
+('7f8eec22-84af-49ef-987d-b8f7c79a16c4', 'Jazz'),
+
+-- The Tote
+('1a3c5e7f-9b2d-4c1e-bf4e-82f54c6a47e3', 'Punk'),
+('1a3c5e7f-9b2d-4c1e-bf4e-82f54c6a47e3', 'Rock'),
+('1a3c5e7f-9b2d-4c1e-bf4e-82f54c6a47e3', 'Hardcore'),
+
+-- Howler
+('d6b74a82-8c5a-41ac-bf29-72591c94f83d', 'Electronic'),
+('d6b74a82-8c5a-41ac-bf29-72591c94f83d', 'Experimental'),
+('d6b74a82-8c5a-41ac-bf29-72591c94f83d', 'Psychedelic'),
+
+-- The Evelyn Hotel
+('4e83c251-56d2-4f8e-8dc0-367e2e87e7d9', 'Indie'),
+('4e83c251-56d2-4f8e-8dc0-367e2e87e7d9', 'Pop'),
+('4e83c251-56d2-4f8e-8dc0-367e2e87e7d9', 'Rock'),
+
+-- The Forum
+('7f2c81f9-0ad9-4a5f-9a08-03f1d7a54021', 'Rock'),
+('7f2c81f9-0ad9-4a5f-9a08-03f1d7a54021', 'Pop'),
+('7f2c81f9-0ad9-4a5f-9a08-03f1d7a54021', 'Alternative'),
+('7f2c81f9-0ad9-4a5f-9a08-03f1d7a54021', 'Electronic'),
+
+-- The Night Cat
+('9e8b39f1-2d4c-4ac0-8d8c-45981c2a8f33', 'Jazz'),
+('9e8b39f1-2d4c-4ac0-8d8c-45981c2a8f33', 'Funk'),
+
+-- Max Watt's House of Music
+('abf3c6e4-22b1-4c63-91f1-40e62e5e9a74', 'Rock'),
+('abf3c6e4-22b1-4c63-91f1-40e62e5e9a74', 'Electronic'),
+('abf3c6e4-22b1-4c63-91f1-40e62e5e9a74', 'Pop'),
+
+-- The Workers Club
+('ce7b6125-bf7f-4c7d-a9de-0a823f13925e', 'Indie'),
+('ce7b6125-bf7f-4c7d-a9de-0a823f13925e', 'Alternative'),
+('ce7b6125-bf7f-4c7d-a9de-0a823f13925e', 'Post Punk'),
+
+-- The Espy
+('f5a91d77-bc94-49de-b627-6c0e01f87363', 'Rock'),
+('f5a91d77-bc94-49de-b627-6c0e01f87363', 'Pop'),
+('f5a91d77-bc94-49de-b627-6c0e01f87363', 'Indie'),
+('f5a91d77-bc94-49de-b627-6c0e01f87363', 'Electronic');
