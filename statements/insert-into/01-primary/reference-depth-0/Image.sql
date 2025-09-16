972b3bbcf3265e4d0f533b00571f993d87ac4b51
MatthewLeigh <matthewcross97@gmail.com>
2025-09-16 12:55:44 +1000

    misc: add images

--- statements/insert-into/01-primary/reference-depth-0/Image.sql
+++ statements/insert-into/01-primary/reference-depth-0/Image.sql
@@ -1,16 +1,88 @@
 INSERT INTO Image (ImageID, URL) 
-VALUES 
-    ('e3a9f7c1-48d2-4fbb-a7b9-54d2c9e3f12a', 'https://www.basicstuffmagazine.com/wp-content/uploads/2020/04/N1Ynur1-.jpg'), -- The Amazing Devil
-    ('b8d9e6f2-7a3c-4f0a-9d18-6e5a7b4c3d2e', 'https://www.philadelphiamusicalliance.org/wp-content/uploads/the-roots-02.jpg'), -- The Roots
-    ('b8d9e6f2-7a3c-4f0a-9d18-6e5a7b4c3d21', 'https://thefader-res.cloudinary.com/images/w_1440,c_limit,f_auto,q_auto:eco/en7ia0j15z4uzb0pp9yg/the-roots-death-of-existentialism.jpg'), -- The Roots
-    ('b8d9e6f2-7a3c-4f0a-9d18-6e5a7b4c3d22', 'https://mg.live/files/images/pages/1527/The%20Roots_2025_Tour_Image_Mobile_800x553.webp?f=png.jpg'), -- The Roots
-    ('d7c1f3a4-982b-4ad7-801e-9c4e5f7b2a1d', 'https://www.mysticsons.com/sites/default/files/styles/large/public/19613/feature/unnamed-700.jpg?itok=kd5BJGra'), -- Tom Misch
-    ('f65d4a8b-c27e-4d2f-b7f2-e1386d5a4e99', 'https://i1.sndcdn.com/avatars-ySVPHIyw8Qbp86uk-YXdGgg-t1080x1080.jpg'), -- Rhsyics
-    ('8a7b5c3e-291f-4a57-8ea1-fd2b3e9c7a0d', 'https://www.rollingstone.com/wp-content/uploads/2019/04/tame-impala-tour.jpg?w=1581&h=1054&crop=1'), -- Tame Impala
-    ('1d2c3b4a-5e6f-7a8b-9c0d-e1f2a3b4c5d6', 'https://images-r2.thebrag.com/rs/uploads/2021/10/gang-of-youths.jpg?resize=940,705&w=940'), -- Gang of Youths
-    ('7e8f9a0b-c1d2-3e4f-5a6b-7c8d9e0f1a2b', 'https://res.akamaized.net/domain/image/fetch/t_web/https://static.domain.com.au/twr/production/uploads/2017/07/20205756/The-Teskey-Brothers5-web-e1500519823818.jpg'), -- The Teskey Brothers
-    ('3b4c5d6e-f7a8-9b0c-1d2e-3f4a5b6c7d8e', 'https://live-production.wcms.abc-cdn.net.au/62f792575b559ddb1ebb35de0bca73d5?impolicy=wcms_crop_resize&cropH=562&cropW=1000&xPos=0&yPos=0&width=862&height=485'), -- Ball Park Music
-    ('2a3b4c5d-6e7f-8a9b-0c1d-2e3f4a5b6c7d', 'https://media.timeout.com/images/103864820/750/422/image.jpg'), -- The Gasometer Hotel
-    ('9f8e7d6c-5b4a-3928-1f0e-dcba98765432', 'https://www.neolutionesport.com/wp-content/uploads/2017/03/default-avatar-knives-ninja.png'), -- Matthew Cross
-    ('c0b1a2d3-e4f5-6789-abcd-ef0123456789', 'https://i.etsystatic.com/51700482/r/il/90cddf/5967617983/il_fullxfull.5967617983_gdx0.jpg'), -- Event Image
-    ('5a6b7c8d-9e0f-1a2b-3c4d-5e6f7a8b9c0d', 'https://i0.wp.com/liveforlivemusic.com/wp-content/uploads/2024/03/rose-on-the-river-fbcover.jpg?resize=740%2C390&ssl=1'); -- Event Banner
+VALUES
+
+-- Artists
+('a13f8c2e-9f54-4c2f-bf16-0a9b3d2e5c71', 'https://f4.bcbits.com/img/a2702896695_16.jpg'), -- The Amazing Devil
+('c4e8b1d7-03b6-4d18-9f90-67b2c21b893f', 'https://preview.redd.it/happy-5-years-of-the-horror-and-the-wild-v0-xmg0zowkl4me1.jpg?width=500&format=pjpg&auto=webp&s=c47d054be1028b48cd8fe33bd37252b91c3e27a8'), 
+('f98c12b4-6e25-45f4-a77f-59e03fd9a312', 'https://www.philadelphiamusicalliance.org/wp-content/uploads/the-roots-02.jpg'), -- The Roots
+('7b1f44a9-df85-4a39-bc3c-8ad0c22c5e48', 'https://hiphopgoldenage.com/wp-content/uploads/2015/07/root.jpg'), 
+('5e9b47c1-b8a2-46fc-8a2a-913c2f76d405', 'https://www.rollingstone.com/wp-content/uploads/2018/06/rs-11002-20130516-roots-x624-1368730973.jpg?w=624&h=420&crop=1'), 
+('d472fa8c-9822-49de-9317-53be2c601b21', 'https://images.squarespace-cdn.com/content/v1/5a5653aeb7411ce23017e460/e758acfe-bc4c-4aa0-ab32-7a31d749ed20/Main+image+-+Credit+-+Charlotte+Patmore.jpg'), -- Tom Misch
+('6f21cd92-8e32-4c0f-b15a-5c44a8a15d27', 'https://troublejuice.co/wp-content/uploads/2022/09/000073-1024x683.jpg'), -- Rhsyics
+('02a6e9d3-0a3d-4c4a-81ab-7f34b10de5c4', 'https://www.undertheradar.co.nz/images/newsImages/tameimpala2012.jpg'), -- Tame Impala
+('d3be7169-90f5-4736-9c65-23ef07ad2a12', 'https://miro.medium.com/v2/resize:fit:1400/1*IfIT4gZ32t9j2thOr46Clg.jpeg'), 
+('8f74cfa1-2f3e-48a1-82b0-31ad1b48b49b', 'https://www.rollingstone.com/wp-content/uploads/2018/06/gang-of-youths-maclay-a92812be-6eb8-4087-8dab-968a55b1b4ba.jpg'), -- Gang of Youths
+('0a71d3f2-7c1e-49d9-94d1-0b4f650fc8d5', 'https://images.sk-static.com/images/media/img/col4/20231205-235521-359634.jpg'), -- The Teskey Brothers
+('d7c5a61f-2a98-4f6b-9a1f-34a78b4c8719', 'https://cdn-images-w3.rrr.org.au/yVNVsEXDfdH8pBUPww7TJ-FvFbo=/1200x628/https://s3.ap-southeast-2.amazonaws.com/assets-w3.rrr.org.au/assets/35c/66b/9ae/35c66b9ae875c916f7ceafebd29e7a7f24ce10e7/the-teskey-brothers-30355374-1-big.jpg'), 
+('59c5b5e1-3a72-48a2-9f04-7130b148fae8', 'https://musicfeeds.com.au/wp-content/uploads/sites/7/ball-park-music-2016.jpg'), -- Ball Park Music
+('2ec8bfb9-4d23-4f97-8103-03c1fa812f57', 'https://beat.com.au/wp-content/uploads/2021/11/Promo-Website-Website-landscape-content-26.jpeg'), -- Courtney Barnett
+('1db7c821-8ac1-44e3-98f2-94e6f4d70a9a', 'https://www.rollingstone.com/wp-content/uploads/2022/02/Vance-Joy-Dont-Fade-Press-Photo.jpg'), -- Vance Joy
+('a90d72c5-1fb7-4a22-9c0d-bcbf71d49a9c', 'https://live-production.wcms.abc-cdn.net.au/29d12e489b3dd4e9efa17a29e688f306?impolicy=wcms_crop_resize&cropH=1688&cropW=3000&xPos=0&yPos=0&width=862&height=485'), -- Flume
+('c7e5d2b1-58ac-44cd-8c7d-0c7a3d6e21f9', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbkMd2mfDfvcZcClZRnE6FZf77o8vF7qwdMg&s'), -- Sampa The Great
+('39b6f4d2-4f67-42d7-9f15-6137cbbd55e3', 'https://internetfm.com/wp-content/uploads/2023/06/KGLW-full-band-e1686347507222.png'), -- King Gizzard & The Lizard Wizard
+('db31af14-6409-4c3c-a3d4-62a8d53cf531', 'https://beat.com.au/wp-content/uploads/2020/11/GIZZARD_PRESS_JULY2020_WDZ-1-scaled-e1605837378906.jpg'), 
+('fa52d6e2-bf3d-48d8-9317-23e09c7b0c8f', 'https://i.guim.co.uk/img/media/db22c0cfda08cc9873252c128d50304e1ea3abed/0_88_3000_1800/master/3000.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=fccdc0a08c382ae0150135384b1dad2a'), 
+('e82c5f3b-1e84-4fc0-8b52-7dca47e1d6c2', 'https://beat.com.au/wp-content/uploads/2017/10/genesis-owusu-768x432.jpg'), -- Genesis Owusu
+('bb84a712-7cd2-4a15-ae09-67b5c84a1f31', 'https://live-production.wcms.abc-cdn.net.au/b6ecfe31ed58573322c89b0e3f60a5b2?impolicy=wcms_crop_resize&cropH=562&cropW=1000&xPos=0&yPos=0&width=862&height=485'), -- Angus & Julia Stone
+('cc59b2e4-4a7c-44f2-8611-88f4b35f71c2', 'https://beat.com.au/wp-content/uploads/2023/05/Ocean-Alley-credit-The-Sauce-e1684801663368.jpg'), -- Ocean Alley
+('d4a8f192-3f41-4f09-8d3a-f92d6fcb1527', 'https://www.nme.com/wp-content/uploads/2021/09/nme-cover-2021-baker-boy-header@2560x1625.jpg'), -- Baker Boy
+('f1a92d5e-9e14-4fb8-91c5-79a2e87c3129', 'https://live-production.wcms.abc-cdn.net.au/ccff98c0f0adaa2c74fbc028e658a468?impolicy=wcms_crop_resize&cropH=562&cropW=1000&xPos=0&yPos=0&width=862&height=485'), -- Middle Kids
+('2a9c1b5d-5c8f-490d-80c4-91c43a0bdfe1', 'https://media.themusic.com.au/images/standard/Artists/D/dz-deathrays/2303-dz-deathrays-david-herington.990x660.jpg'), -- DZ Deathrays
+('3e4b7a2f-60b1-42fc-8f59-90c1f2b6e4c7', 'https://www.slugmag.com/wp/wp-content/uploads/2022/10/the-jungle-giants-SLUG-2022.png'), -- The Jungle Giants
+('41e7bfc2-04f1-4af8-bfb4-8f1c0d3a2e56', 'https://www.emimusic.com.au/wp-content/uploads/sites/7/1986/04/A7A1926_v2_RT.jpg'), -- Meg Mac
+('56f92d84-9a3f-4209-b21d-53c7a61c1f8a', 'https://live-production.wcms.abc-cdn.net.au/e397763e5ff7fb319801b4399a31c283?impolicy=wcms_crop_resize&cropH=1400&cropW=2486&xPos=14&yPos=178&width=862&height=485'), -- Alex Lahey
+('6ac4e8f1-3c2d-41b1-bf48-9f47e1b2d9a3', 'https://i0.wp.com/theheadstream.com/wp-content/uploads/2022/01/stella-donnelly-edited.jpeg?resize=940%2C936&ssl=1'), -- Stella Donnelly
+('7be5d491-2d5a-472a-8c75-64e2c9b3d2a1', 'https://subpop-img.s3.amazonaws.com/asset/artist_images/attachments/000/009/442/max_600_400/rollingblackoutscoastalfever-2022-promo-03-nickmckk-2250x1500-300.jpg?1649192865'), -- Rolling Blackouts Coastal Fever
+('8ca2f3e6-9f01-42a9-b2c8-1f23a6d7e54c', 'https://www.undertheradar.co.nz/images/newsImages/hockeydadtour20241240.jpg'), -- Hockey Dad
+('9b4f21d2-5e34-45a3-8f5e-7a6d3c2b1e90', 'https://live-production.wcms.abc-cdn.net.au/697f79a6e920aa63794ff11a2fc7edf0?impolicy=wcms_crop_resize&cropH=641&cropW=1138&xPos=1&yPos=0&width=862&height=485&imformat=generic'), -- Mallrat
+('ad1f4b27-3a2e-49d9-b3f4-0f8e5a1d2b37', 'https://live-production.wcms.abc-cdn.net.au/629afa266992add6c695627ea922104c?impolicy=wcms_crop_resize&cropH=562&cropW=1000&xPos=0&yPos=0&width=862&height=485'), -- G Flip
+('be9a2f16-4c27-48d0-8a15-1d6f7b2a3e45', 'https://beat.com.au/wp-content/uploads/2022/09/Untitled-design-6.png'), -- Thelma Plum
+('cf3e9a54-5e2f-4d92-bc9f-7a3e4f8d1b29', 'https://live-production.wcms.abc-cdn.net.au/369039ff599d488abf5cad6999509124?impolicy=wcms_crop_resize&cropH=608&cropW=1080&xPos=0&yPos=70&width=862&height=485'), -- Tones And I
+('d1f7b4e9-7a6c-4c3f-b5f1-2e8a7c1d4e36', 'https://f4.bcbits.com/img/0025309790_10.jpg'), -- Amber Drift
+('e2c5a8d3-9b7e-4f15-924f-5a3c8d7f2b91', 'https://assets-au-01.kc-usercontent.com/a74cc67d-6861-022b-4d6d-57679e9d331f/e49ed7a9-9304-42b7-92f1-fe8011952620/Gav_Singing.jpg'),
+
+--- Venues
+('2a3b4c5d-6e7f-8a9b-0c1d-2e3f4a5b6c7d', 'https://media.timeout.com/images/103864820/750/422/image.jpg'), -- The Gasometer Hotel
+('3a4b5c6d-7e8f-901a-b2c3-d4e5f6a7b8c9', 'https://media.cntraveler.com/photos/5b844aca95e9b50555654418/16:9/w_2560,c_limit/Corner-Hotel_Andrew-Ashton_CornerHotel_Exterior.jpg'), -- Corner Hotel
+('4b5c6d7e-8f90-1a2b-3c4d-5e6f7a8b9c0d', 'https://www.visitvictoria.com/-/media/images/melbourne/food-and-wine/pubs/northcote-social-club/northcote-social-club-northcote_mel_r_josie-withers_132662_1600x1200.jpg?ts=20240507441127'), -- Northcote Social Club
+('5c6d7e8f-901a-2b3c-4d5e-6f7a8b9c0d1e', 'https://media.cntraveler.com/photos/5de69e23083c7000096590ec/16:9/w_2560,c_limit/The-Tote_2019_Tahlia-Palmer_TheTote_2015_08_Tahlia_Palmer.jpg'), -- The Tote
+('6d7e8f90-1a2b-3c4d-5e6f-7a8b9c0d1e2f', 'https://cdn.broadsheet.com.au/cache/55/9d/559d1ccc4509cb9e605199c2e5670ab7.jpg'), -- Howler
+('7e8f901a-2b3c-4d5e-6f7a-8b9c0d1e2f3a', 'https://thehappiesthour.s3.ap-southeast-2.amazonaws.com/media/1822/evelyn-hotel.jpg'), -- The Evelyn Hotel
+('8f901a2b-3c4d-5e6f-7a8b-9c0d1e2f3a4b', 'https://forummelbourne.com.au/uploads/slider/163/forum-auditorium-gallery-5-1510111834-1702338790.jpeg'), -- The Forum
+('9a01b2c3-4d5e-6f7a-8b9c-0d1e2f3a4b5c', 'https://beat.com.au/wp-content/uploads/2019/05/Mildlife1-cred-David-Harris-e1614131917736.jpg'), -- The Night Cat
+('0b1c2d3e-4f5a-6b7c-8d9e-0f1a2b3c4d5e', 'https://therockerrag.com/wp-content/uploads/2021/07/KingParrot114-copy-1-1024x669.jpg'), -- Max Watt's House of Music
+('1c2d3e4f-5a6b-7c8d-9e0f-1a2b3c4d5e6f', 'https://www.theworkersclub.com.au/uploads/5/0/7/7/50772367/1656957_orig.jpg'), -- The Workers Club
+('2d3e4f5a-6b7c-8d9e-0f1a-2b3c4d5e6f70', 'https://media.timeout.com/images/105918773/750/422/image.jpg'), -- The Espy
+
+--- Events
+('b3e2c1d4-5f6a-4b7c-8d9e-0f1a2b3c4d5e', 'https://m.media-amazon.com/images/I/61k8WqCQh2L._UF894,1000_QL80_.jpg'), -- Event 1
+('c4d5e6f7-8a9b-0c1d-2e3f-4a5b6c7d8e9f', 'https://marketplace.canva.com/EAFIygYzkes/1/0/1131w/canva-blue-minimalist-concert-music-cover-poster-CGNgQz4KqL0.jpg'), -- Event 2
+('d5e6f7a8-9b0c-1d2e-3f4a-5b6c7d8e9f01', 'https://marketplace.canva.com/EAF6QeL9pmc/2/0/1131w/canva-orange-and-red-vintage-music-festival-poster-Fap2AkFKwbc.jpg'), -- Event 3
+('e6f7a8b9-0c1d-2e3f-4a5b-6c7d8e9f0123', 'https://i.ebayimg.com/images/g/P9QAAOSwo-Vb~rTB/s-l400.jpg'), -- Event 4
+('f7a8b90c-1d2e-3f4a-5b6c-7d8e9f012345', 'https://collection.australiansportsmuseum.org.au/records/images/xlarge/36244/7baec06e9e676c9f6faaf701c63f0d494a73ea65.jpg'), -- Event 5
+('08b9c0d1-2e3f-4a5b-6c7d-8e9f01234567', 'https://i.ebayimg.com/images/g/HV4AAOSwdkZbEcGy/s-l1200.jpg'), -- Event 6
+('19c0d1e2-3f4a-5b6c-7d8e-9f0123456789', 'https://www.rarerecords.com.au/cdn/shop/products/JET-Thee-Get-Well-Gone-Revue-2004-Tour-Poster.jpg?v=1658503679'), -- Event 7
+('2ad1e2f3-4a5b-6c7d-8e9f-0123456789ab', 'https://cdn.create.vista.com/downloads/071ecdba-c456-4ecf-bd36-2fe69b2b87b2_640.jpeg'), -- Event 8
+('3be2f304-5b6c-7d8e-9f01-23456789abcd', 'https://marketplace.canva.com/EAFSIoENUXI/1/0/1131w/canva-black-minimalist-concert-tour-dates-announcement-poster-Xjk-UKZ8PNc.jpg'), -- Event 9
+('4cf30415-6c7d-8e9f-0123-456789abcde0', 'https://www.liveoar.com/cdn/shop/files/OAR_TourPoster_2024.jpg?v=1724966934&width=1080'), -- Event 10
+('5df41526-7d8e-9f01-2345-6789abcdef01', 'https://media.gettyimages.com/id/1937316632/vector/rock-poster.jpg?s=612x612&w=gi&k=20&c=TofYumXS3SylKqy1gsglB2N1nfa8OLzc-XPpQfRQGqw='), -- Event 11
+('6ef52637-8e9f-0123-4567-89abcdef0123', 'https://i.pinimg.com/736x/31/b5/b8/31b5b8dbcd63e2870f8d40d73d8430c4.jpg'), -- Event 12
+('70f63748-9f01-2345-6789-abcdef012345', 'https://i.ebayimg.com/images/g/ADEAAOSwFe5jNoxt/s-l400.jpg'), -- Event 13
+('81f74859-0123-4567-89ab-cdef01234567', 'https://design-assets.adobeprojectm.com/content/download/express/public/urn:aaid:sc:VA6C2:05d27390-13dd-5739-8796-712e0a82bc39/component?assetType=TEMPLATE&etag=84c5d8b9cecb4acfac3ef86f51053e31&revision=82e0c78c-edb9-4eea-9e7e-c23857224deb&component_id=c0ca9e3a-5c2b-4ee2-b75c-0b4f430b9d26'), -- Event 14
+('92f8596a-1234-5678-9abc-def012345678', 'https://derekprice.net/wp-content/uploads/POSTER_RICKDANKO.jpg'), -- Event 15
+('a3f96a7b-2345-6789-abcd-ef0123456789', 'https://www.prestoimages.net/store20/rd2871/2871_pd2193459_1.jpg'), -- Event 16
+('b4fa7b8c-3456-789a-bcde-f0123456789a', 'https://i.etsystatic.com/35599031/r/il/f8de7e/3881457678/il_570xN.3881457678_e91t.jpg'), -- Event 17
+('c5fb8c9d-4567-89ab-cdef-123456789abc', 'https://www.designyourway.net/blog/wp-content/uploads/2017/09/c3808064e6e5fa8dd489ee6c940.jpg'), -- Event 18
+('d6fc9dae-5678-9abc-def0-23456789abcd', 'https://mir-s3-cdn-cf.behance.net/project_modules/1400_webp/98cbc8187040239.6580835d10e4c.jpg'), -- Event 19
+('e7fdabcf-6789-abcd-ef01-3456789abcde', 'https://posterspy.com/wp-content/uploads/2023/07/Orgone-3D-Flat.jpg'), -- Event 20
+('f8febcd0-789a-bcde-f012-456789abcdef', 'https://www.bluedogposters.com.au/cdn/shop/files/the1975_1400x.jpg?v=1697759746'), -- Event 21
+('09ffcd01-89ab-cdef-1234-56789abcdef0', 'https://www.valverecords.com.au/shop/wp-content/uploads/2024/01/OFFaustraliantourposter2024signed.jpg'), -- Event 22
+('1a00de12-9abc-def0-2345-6789abcdef01', 'https://upload.wikimedia.org/wikipedia/en/6/64/Tate_McRae_-_Think_Later_World_Tour_poster.jpg'), -- Event 23
+('2b01ef23-abcd-ef01-3456-789abcdef012', 'https://www.rollingstone.com/wp-content/uploads/2018/06/p37_pixiesfinal-5df7a9f6-01f9-46d1-9449-feaf4e3a4442.jpg?w=450'), -- Event 24
+('3c120f34-bcde-f012-4567-89abcdef0123', 'https://www.shutterstock.com/image-vector/retro-vintage-styled-psychedelic-rock-600w-2292040729.jpg'), -- Event 25
+('4d231045-cdef-1234-5678-9abcdef01234', 'https://d1vlmco8ur52ud.cloudfront.net/10-10-2024-041930-9493.jpg'), -- Event 26
+('5e342156-def0-2345-6789-abcdef012345', 'https://lh4.googleusercontent.com/proxy/IE2_CurdvQuQy8kxAt6o5KwtkeX19mITYPeqt5IgaH-QhFsOsoCGMYvarASuTJ3slWiRMQ0Nf_pTcrBEHPdBJnaCevzf4ct9bjn5VVbDqMI'), -- Event 27
+('6f453267-ef01-3456-789a-bcdef0123456', 'https://i.ebayimg.com/images/g/p~oAAeSwqahotcQT/s-l400.jpg'), -- Event 28
+('70564378-f012-4567-89ab-cdef01234567', 'https://i.ebayimg.com/images/g/ASMAAOSwR-9eNHMf/s-l400.jpg'), -- Event 29
+('81675489-0123-5678-9abc-def012345678', 'https://beyondthepale.com.au/cdn/shop/products/ramonesAussietour_1024x1024.jpg?v=1601358249'), -- Event 30
+('9278659a-1234-6789-abcd-ef0123456789', 'https://design-assets.adobeprojectm.com/content/download/express/public/urn:aaid:sc:VA6C2:545cda1c-8386-5996-bc55-1be6b53e36a5/component?assetType=TEMPLATE&etag=4c3030614b364256b95495e3c2323042&revision=08d99a4f-5f73-4e4b-ad1e-fedfa5afa198&component_id=875ea2b5-cb42-4bda-9dcc-a2595415e41d'); -- Event 31
